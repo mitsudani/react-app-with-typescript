68948622d338f71727a8b71d2e77242cb02ceca7
Mitsudani <mdsachetti@gmail.com>
2022-06-28 16:58:11 -0300

    interfaces and List component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import List from "./components/List";
+
+interface Sub {
+  nick: string;
+  avatar: string;
+  subMonths: number;
+  description?: string;
+}
+
+interface AppState {
+  subs: Array<Sub>;
+  newSubsNumber: number;
+}
+
+const INITIAL_STATE = [
+  {
+    nick: "dapelu",
+    subMonths: 3,
+    avatar: "https://i.pravatar.cc/150?u=dapelu",
+    description: "Dapelu hace de moderador a veces",
+  },
+  {
+    nick: "sergio_serrano",
+    subMonths: 7,
+    avatar: "https://i.pravatar.cc/150?u=sergio_serrano",
+  },
+];
 
 function App() {
-  const [subs, setSubs] = useState([
-    {
-      nick: "dapelu",
-      subMonths: 3,
-      avatar: "https://i.pravatar.cc/150?u=dapelu",
-      description: "Dapelu hace de moderador a veces",
-    },
-    {
-      nick: "sergio_serrano",
-      subMonths: 7,
-      avatar: "https://i.pravatar.cc/150?u=sergio_serrano",
-    },
-  ]);
+  const [subs, setSubs] = useState<AppState["subs"]>([]);
+  const [newSubsNumber, setNewSubsNumber] =
+    useState<AppState["newSubsNumber"]>(0);
+
+  useEffect(() => {
+    setSubs(INITIAL_STATE);
+  }, []);
 
   return (
     <div className="App">
       <h1>Midu subs</h1>
-      <ul>
-        {subs.map((sub) => {
-          return (
-            <li key={sub.nick}>
-              <img src={sub.avatar} alt={`Avatar for ${sub.nick}`} />
-              <h4>
-                {sub.nick} (<small>{sub.subMonths}</small>)
-              </h4>
-              <p>{sub.description?.substring(0, 100)}</p>
-            </li>
-          );
-        })}
-      </ul>
+      <List subs={subs} />
     </div>
   );
 }
